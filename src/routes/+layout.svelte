d874b0d09bda8912fea020f834126882508a12f7
BijSven <mail@bijsven.nl>
2025-07-16 14:04:23 +0200

    fix(theme): improved theme handling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,13 @@
 <script>
 	import '../app.css';
 
-	import { ModeWatcher } from 'mode-watcher';
+	import { ModeWatcher, mode } from 'mode-watcher';
 	import { Toaster } from 'svelte-sonner';
 
 	const { children } = $props();
 </script>
 
 <ModeWatcher defaultMode="system" />
-<Toaster position="top-right" theme="system" richColors />
+<Toaster position="top-right" theme={$mode ? 'light' : 'dark'} richColors />
 
 {@render children()}
