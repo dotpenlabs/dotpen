802025c2591f0ab32d8652dd1eee625fa0dfea45
BijSven <mail@bijsven.nl>
2025-07-17 20:25:28 +0200

    ui(console): Console decorations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,35 @@
 
 	import { ModeWatcher, mode } from 'mode-watcher';
 	import { BugBeetle } from 'phosphor-svelte';
+	import { onMount } from 'svelte';
 	import { Toaster } from 'svelte-sonner';
 
 	const { children } = $props();
+
+	onMount(() => {
+		console.log(
+			`%c
+
+ /$$$$$$$            /$$                               
+| $$__  $$          | $$                               
+| $$  \  $$ /$$$$$$ /$$$$$$   /$$$$$$  /$$$$$$ /$$$$$$$ 
+| $$  | $$/$$__  $|_  $$_/  /$$__  $$/$$__  $| $$__  $$
+| $$  | $| $$  \  $$ |$$   | $$  \  $| $$$$$$$|  $$  \  $$
+| $$  | $| $$  | $$ | $$ /$| $$  | $| $$_____| $$   |$$
+| $$$$$$$|  $$$$$$/ |  $$$$| $$$$$$$|  $$$$$$| $$   |$$
+|_______/ \______/   \___/ | $$____/ \_______|__/  |__/
+                           | $$                        
+                           | $$                        
+                           |__/                        
+
+
+Dotpen Labs (C) ${new Date().getFullYear()}
+
+Watch out! Pasting code into the development console can be dangerous and is not recommended. If you know what you're doing, come help us out iwanttodevelopdotpen@bijsven.nl
+`,
+			`font-family: monospace`
+		);
+	});
 </script>
 
 <ModeWatcher defaultMode="light" />
