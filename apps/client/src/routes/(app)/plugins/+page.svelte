dedaaadda3c9b0bee9de4d130f7e7134293cfdfe
BijSven <mail@bijsven.nl>
2025-07-25 15:17:39 +0200

    feat: updated mgr for new plugin platform

--- apps/client/src/routes/(app)/plugins/+page.svelte
+++ apps/client/src/routes/(app)/plugins/+page.svelte
@@ -1,15 +1,25 @@
 <script>
-	import { plgmgr } from '$/lib/plugins/mgr';
+	import { PluginKit } from '$/lib/plugins/head';
 	import View from '$/lib/plugins/view.svelte';
 	import { onMount } from 'svelte';
 
-	plgmgr.register({
-		name: 'main',
-		url: '/plugins/simple/page.html',
-		location: 'main'
+	onMount(() => {
+		try {
+			PluginKit.deregister('dropbox');
+
+			PluginKit.register({
+				id: 'dropbox',
+				url: '/plugins/dropbox/page.html',
+				location: 'sidebar-bottom',
+
+				perms: ['app:sidebar-bottom']
+			});
+		} catch (e) {
+			console.error(e);
+		}
 	});
 </script>
 
-<content class="bg-red-500 h-full w-full absolute">
-	<View location="main" />
+<content class="h-full w-full absolute">
+	<p>New plugins are now loaded!</p>
 </content>
