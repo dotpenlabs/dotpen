0385153c5162d35d84ef633f3dbba218cb7f254d
BijSven <mail@bijsven.nl>
2025-07-29 14:52:57 +0200

    chore: moved Window types from +layout.svelte to app.d.ts

--- apps/client/src/app.d.ts
+++ apps/client/src/app.d.ts
@@ -5,6 +5,11 @@ declare module 'macy';
 declare global {
 	const APP_VERSION: string;
 
+	interface Window {
+		SetHydrating: (value: boolean) => void;
+		SetColored: (isColored: 'default' | 'green' | 'red') => void;
+	}
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
