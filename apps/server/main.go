7c64720dd4a0a455a56775828169d437f4b5f506
BijSven <mail@bijsven.nl>
2025-09-20 09:44:25 +0200

    feat(server): removed waitlist, added additional length for deleted items sync.
    TODO: Add dynamic delete states, where all logged in devices need to have connected to the server until the deleted items are done. Blocked by full-app-sync on open and server not knowing with devices are logged in, requires an additional table.

--- apps/server/main.go
+++ apps/server/main.go
@@ -48,24 +48,6 @@ func main() {
 			return nil
 		}).Bind(apis.RequireAuth())
 
-		se.Router.GET("/api/waitlist-members", func(e *core.RequestEvent) error {
-			var result struct {
-				Count int `db:"c"`
-			}
-
-			err := e.App.DB().
-				NewQuery("SELECT COUNT(*) AS c FROM waitlist").
-				One(&result)
-			if err != nil {
-				return err
-			}
-
-			e.Response.WriteHeader(200)
-			e.Response.Write([]byte(strconv.Itoa(result.Count)))
-
-			return nil
-		})
-
 		se.Router.GET("/{path...}", apis.Static(os.DirFS("./public"), false))
 		se.Router.GET("/_/images/{path...}", func(e *core.RequestEvent) error {
 			staticFS := os.DirFS("./public/assets")
@@ -80,47 +62,6 @@ func main() {
 		return se.Next()
 	})
 
-	app.OnRecordCreateRequest("users").BindFunc(func(e *core.RecordRequestEvent) error {
-		if e.HasSuperuserAuth() {
-			return e.Next()
-		}
-
-		wli, err := app.FindFirstRecordByFilter("waitlist", "email = '"+e.Record.Email()+"'")
-		if err != nil {
-			if strings.Contains(err.Error(), "no rows in result set") {
-				return apis.NewApiError(400, "user is not accepted on the waitlist", "WaitlistNoAccept")
-			}
-			return err
-		}
-
-		if !wli.GetBool("accept") {
-			return apis.NewApiError(400, "user is not accepted on the waitlist", "WaitlistNoAccept")
-		}
-
-		return e.Next()
-	})
-
-	app.OnRecordUpdate("waitlist").BindFunc(func(e *core.RecordEvent) error {
-		if e.Record.GetBool("accept") {
-			emailBytes, err := emails.ReadFile("emails/invited.html")
-			if err != nil {
-				return err
-			}
-
-			message := &mailer.Message{
-				From: mail.Address{
-					Address: e.App.Settings().Meta.SenderAddress,
-					Name:    e.App.Settings().Meta.SenderName,
-				},
-				To:      []mail.Address{{Address: e.Record.GetString("email")}},
-				Subject: "🎉 You've been invited to join Dotpen!",
-				HTML:    string(emailBytes),
-			}
-
-			e.App.NewMailClient().Send(message)
-		}
-		return e.Next()
-	})
 
 	app.OnRecordUpdateExecute("bookmarks").BindFunc(func(e *core.RecordEvent) error {
 		app.Logger().Debug("RecordUpdate: bookmarks", "action", "update")
@@ -138,7 +79,7 @@ func main() {
 		return e.Next()
 	})
 
-	app.Cron().MustAdd("Remove deleted bookmarks", "0 0 * * *", func() {
+	app.Cron().MustAdd("Remove deleted bookmarks", "0 0 * * 1", func() {
 		app.Logger().Debug("Cron: Remove deleted bookmarks")
 
 		records, err := app.FindAllRecords("bookmarks",
